3cafa2824a8af6e445376a816b7ef985f9fdefe9
kek <oklikhunter@mail.ru>
2018-03-15 18:29:28 +0300

    hz

--- Dream_Team/Dream_Team/Source.cpp
+++ Dream_Team/Dream_Team/Source.cpp
@@ -3,13 +3,14 @@
 #include "tgaimage.h"
 #include "Painter.h"
 #include <iostream>
+#include "Render.h"
 
 const char* dir = "obj/african_head.obj";
 
 const PNGColor white = PNGColor(255, 255, 255, 255);
 const PNGColor red = PNGColor(255, 255, 0, 0);
 const PNGColor blue = PNGColor(255, 0, 0, 255);
-const int width = 1600;
+const int width = 800;
 const int height = 800;
 PNGImage* image = NULL;
 
@@ -19,10 +20,10 @@ int main() {
 		std::cout << "#vert " << model.vertsSize() << " #faces " << model.facesSize() << std::endl;
 
 		image = new PNGImage(width, height, PNGType::RGB);
-		Painter paint(image);
-
+		//Painter paint(image);
+		//Render render = new Render(image, );
 		for (int i = 0; i < model.facesSize(); i++) {
-			paint.polygon(model.verts(model.face(i).at(0)),red , width / 4, height /2);
+			//paint.polygon(model.verts(model.face(i).at(0)),red , width / 2, height /2);
 		}
 
 		/*Point2dF start(width / 2, height / 2);
