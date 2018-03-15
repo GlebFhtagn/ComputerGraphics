3de9a2f68afba3c1c20d1c26c297fdb3ad290913
kek <oklikhunter@mail.ru>
2018-03-15 18:04:34 +0300

    risuem treugolniky

--- Dream_Team/Dream_Team/Render.h
+++ Dream_Team/Dream_Team/Render.h
@@ -5,10 +5,14 @@
 class Render
 {
 public:
-	Render(PNGImage* _image, std::vector<std::vector<Point3dF>> _polygons);
+	Render(PNGImage* _image, std::vector<Triangle> _polygons);
 	~Render();
+	void Draw_triangle(Triangle triangle);
 private:
 	PNGImage* image;
-	std::vector<std::vector<Point3dF>> polygons;
+	std::vector<Triangle> polygons;
+	std::vector<float> get_barycentric_coordinates(Triangle triangle,int x,int y);
+	int max_of_three(int x,int y,int z);
+	int min_of_three(int x, int y, int z);
 };
 
